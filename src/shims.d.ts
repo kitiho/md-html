6e8d1daa928c8524e2f285bf60def0bf38349475
enoshima7 <duanjipeng0328@gmail.com>
2022-09-14 23:11:46 +0800

    feat: eslint

---
+++ src/shims.d.ts
@@ -0,0 +1,4 @@
+declare module 'markdown-it-br' {
+  const mdBr : any;
+  export default mdBr
+}
--- test/basic.test.ts
+++ test/basic.test.ts
@@ -1,12 +1,12 @@
-import { readFile } from 'fs/promises';
-import path from 'node:path';
-import { assert, expect, test,it } from 'vitest'
-import {markdownToHtml} from '../src/index'
+import { readFile } from 'fs/promises'
+import path from 'node:path'
+import { assert, expect, it, test } from 'vitest'
+import { markdownToHtml } from '../src/index'
 
-it('basic', async() => {
-  const md = await readFile(path.resolve(__dirname, 'test.md'),{
-    encoding:'utf-8'
-  });
+it('basic', async () => {
+  const md = await readFile(path.resolve(__dirname, 'test.md'), {
+    encoding: 'utf-8',
+  })
   const output = markdownToHtml(md)
   expect(output).toMatchInlineSnapshot(`
     "<p>hello <br> world</p>
